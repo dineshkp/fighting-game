55256bd1679a4eb15599a9e419ad3a759a955765
Dinesh Kumar P <64963503+dineshkp@users.noreply.github.com>
2023-11-09 10:46:42 +0530

    fix: buggy attackbox in enemy
    modify: update animation timing for attackbox when no collision

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ class Sprite {
         this.isAttacking = true;
         setTimeout(() => {
             this.isAttacking = false;
-        }, 1000)
+        }, 100)
     }
 }
 
@@ -114,8 +114,8 @@ const keys = {
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
     return (
-        rectangle1.position.x + rectangle1.attackBox.width >= rectangle2.position.x
-        && rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
+        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
         && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
         && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
     );
