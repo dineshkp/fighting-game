9d9fed83052033a404e115c6f941ff22738fbd13
Dinesh Kumar P <64963503+dineshkp@users.noreply.github.com>
2023-12-13 14:30:54 +0530

    modify: add shop sprite animation

--- src/js/classes/Sprite.js
+++ src/js/classes/Sprite.js
@@ -7,12 +7,15 @@ class Sprite {
     this.image.src = imageSrc;
     this.scale = scale;
     this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
   }
 
   draw() {
     c.drawImage(
         this.image,
-        0,
+        (this.image.width / this.framesMax) * this.framesCurrent,
         0,
         this.image.width / this.framesMax,
         this.image.height,
@@ -25,5 +28,14 @@ class Sprite {
 
   update() {
     this.draw();
+    this.framesElapsed++;
+
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.framesCurrent = 0;
+      }
+    }
   }
 }
